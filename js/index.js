f849208b63e0af564b5f289ee8da11de47df8a24
orsoli <fljcoli@gmail.com>
2024-08-11 09:46:44 +0200

    add nav Bar links functions

--- js/index.js
+++ js/index.js
@@ -6,14 +6,51 @@
 // Retrieve interested elements from DOM
 const burgerMenuElement = document.getElementById('burger-menu');
 const navMenuElement = document.getElementById('navbar-menu');
+const navLinkElements = document.querySelectorAll(".nav-link");
+const dropDownMenuElements = document.querySelectorAll(".dropdown-menu");
+const navBarChevronDownElements = document.querySelectorAll(".navbar-chevronDown")
+const navBarChevronDownHoverElements = document.querySelectorAll(".navbar-chevronDown-hover")
 
 
-// -- Define functions
+// --- Processing phase
+//  Define functions
 // Hamburger menu click handling function
 function burgerClickHandling(){
     // TNav menu dusplay
     navMenuElement.classList.toggle('collapse')
 }
-// --- Processing phase
+// --- Loping phase
+// Ceate for loop to pass in each navlink element
+for(i = 0; i < navLinkElements.length; i++){
+    // Save each dropdown element
+    const dropDownMenuElement = dropDownMenuElements[i]
+    // Save each navlink element
+    const navLinkElement = navLinkElements[i]
+    // Save every navbar chevrondown element
+    const navBarChevronDownElement = navBarChevronDownElements[i];
+    // Save every navbar chevrondown hover element
+    const navBarChevronDownHoverElement = navBarChevronDownHoverElements[i];
+    // add an event listener 'mouse over' to navbar chevrondown
+    navLinkElement.addEventListener('mouseover', function(){
+        // Mouse over event function
+        navBarChevronDownElement.classList.toggle('d-none')
+        navBarChevronDownHoverElement.classList.toggle('d-none')
+        dropDownMenuElement.classList.toggle('collapse')
+    })
+    // Add an event listener 'mouse out' to navbar chevrondown
+    navLinkElement.addEventListener('mouseout', function(){
+        // Mouse over event function
+        navBarChevronDownElement.classList.toggle('d-none')
+        navBarChevronDownHoverElement.classList.toggle('d-none')
+        dropDownMenuElement.classList.toggle('collapse')
+    })
+
+    // Add an 'click' event listener 
+    navLinkElement.addEventListener('click', function(e){
+        e.preventDefault();
+        dropDownMenuElement.classList.toggle('collapse')
+    })
+}
+
 // Add Burger menu click event listener
 burgerMenuElement.addEventListener('click', burgerClickHandling)
